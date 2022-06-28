7fe16010c72bd8b15a127db0a6e4c507826b4e2f
Yousaf <yousaf@yolbe.com>
2022-06-29 00:40:45 +0100

    added frontend project files to repository

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./css/index.css";
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
